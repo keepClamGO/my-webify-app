6a58b3bfa1d17bcd4a593eb2fa51235a0dafc537
keeping <469044001@qq.com>
2022-05-11 15:05:36 +0800

    commit task2

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -3,7 +3,7 @@ import { Button } from 'antd';
 import { Link } from "react-router-dom";
 
 const Login: React.FC = () => <div> 这里是login页  点击跳转 <Button type="link"><Link to={'/BasicLayout/Home'}>
-home
+go home
 </Link></Button> </div>;
 
 export default Login;
